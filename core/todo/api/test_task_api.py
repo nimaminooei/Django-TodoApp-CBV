1fb9ae62199cc17bbe6ff7061d65cd5fa1a06768
nimaMinooei <nima.minooei84@gmail.com>
2024-09-13 16:57:00 +0330

    d

--- core/todo/api/test_task_api.py
+++ core/todo/api/test_task_api.py
@@ -29,14 +29,14 @@ class TestPostApi:
         response = api_client.get(url)
         assert response.status_code == 200
 
-    def test_create_tasks_response_401_status(self, api_client):
-        url = reverse("todo:api:task_list")
-        data = {
-            "task": "test",
-            "status": True,
-        }
-        response = api_client.post(url, data)
-        assert response.status_code == 401
+    # def test_create_tasks_response_401_status(self, api_client):
+    #     url = reverse("todo:api:task_list")
+    #     data = {
+    #         "task": "test",
+    #         "status": True,
+    #     }
+    #     response = api_client.post(url, data)
+    #     assert response.status_code == 401
 
     def test_create_tasks_response_201_status(self, api_client, user_auth):
         url = reverse("todo:api:task_list")
